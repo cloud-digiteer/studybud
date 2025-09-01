38d71d59c51a4c5e9723b2080a973addf1e582bc
cloudneo0719@gmail.com <cloudneo0719@gmail.com>
2025-09-01 16:08:36 +0800

    Add Templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id':1, 'name':'Lets learn py!'},
+    {'id':2, 'name':'Design With Me!'},
+    {'id':3, 'name':'Frontend Developers!'},
+    
+]
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'base/home.html', {'rooms':rooms})
+
+def room(request, pk):
+    room = None 
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    return render(request, 'base/room.html', {'room': room})
