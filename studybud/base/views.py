606a72ab2875263569168ba57307c7224751b48d
cloudneo0719@gmail.com <cloudneo0719@gmail.com>
2025-09-02 17:08:06 +0800

    Add CRUD operations

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
@@ -8,3 +9,35 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     return render(request, 'base/room.html', {'room': room})
+
+def create_room(request):
+    form = RoomForm()
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+                
+    context = {"form": form}
+    return render(request, 'base/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {"form": form}
+    return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
